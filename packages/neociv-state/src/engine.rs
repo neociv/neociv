5061408edefa1e80732829c38514141303004b91
murt <murt@unpossible.io>
2022-02-03 14:10:10 +1000

    #2: Added tests for adding / removing civs and using the cloning into immutable state

--- packages/neociv-state/src/engine.rs
+++ packages/neociv-state/src/engine.rs
@@ -1,11 +1,15 @@
 use crate::state::NeocivState;
-use crate::civ::Civ;
+use crate::civ::{Civ,CivId};
 
+#[derive(Debug)]
 pub enum StateError {
     DuplicateCivId,
     InvalidCivId,
+    UnknownCivId,
 }
 
+/// Any modification to the state produces a result that either contains the successfully updated
+/// state *or* fails with a specific StateError.
 pub type StateResult = Result<NeocivState, StateError>;
 
 /// Initialise an empty & default state.
@@ -14,14 +18,31 @@ pub fn init() -> NeocivState {
 }
 
 /// Add a Civ to the state.
-pub fn add_civ(mut state: NeocivState, civ: Civ ) -> StateResult {
+pub fn add_civ(state: NeocivState, civ: Civ ) -> StateResult {
     if state.civs.iter().any(|i| i.id == civ.id) {
         return Err(StateError::DuplicateCivId);
     } else if civ.id.len() == 0 {
         return Err(StateError::InvalidCivId);
     } else {
-        state.civs.push(civ);
-        return Ok(state);
+        let mut new_state = state.clone();
+        new_state.civs.push(civ);
+        return Ok(new_state);
+    }
+}
+
+/// Remove a Civ from the state - including from all ownership roles and owned units
+pub fn remove_civ(state: NeocivState, civ_id: CivId) -> StateResult {
+    if civ_id.len() == 0 {
+        return Err(StateError::InvalidCivId);
+    } else if !state.civs.iter().any(|i| i.id == civ_id) {
+        return Err(StateError::UnknownCivId);
+    } else  {
+        let mut new_state = state.clone();
+        // TODO: Remove ownership of all cells
+        // TODO: Remove ownership of all units
+        // Remove from the list of civs
+        new_state.civs.retain(|c| c.id != civ_id);
+        return Ok(new_state);
     }
 }
 
@@ -38,7 +59,26 @@ mod tests {
     fn test_add_civ() {
         let mut state: crate::state::NeocivState = crate::engine::init();
         assert_eq!(state.civs.len(), 0);
-        state = crate::engine::add_civ(state, crate::civ::Civ { id: String::from("example"), title: String::from("Example") });
+        state = match crate::engine::add_civ(state, crate::civ::Civ { id: String::from("example"), title: String::from("Example") }) {
+            Ok(state) => state,
+            Err(ex) => panic!("{:?}", ex),
+        };
         assert_eq!(state.civs.len(), 1);
+    }
+
+    #[test]
+    fn test_remove_civ() {
+        let mut state: crate::state::NeocivState = crate::engine::init();
+        assert_eq!(state.civs.len(), 0);
+        state = match crate::engine::add_civ(state, crate::civ::Civ { id: String::from("example"), title: String::from("Example") }) {
+            Ok(state) => state,
+            Err(ex) => panic!("{:?}", ex),
+        };
+        assert_eq!(state.civs.len(), 1);
+        state = match crate::engine::remove_civ(state, String::from("example")) {
+            Ok(state) => state,
+            Err(ex) => panic!("{:?}", ex),
+        };
+        assert_eq!(state.civs.len(), 0);
     }
 }
