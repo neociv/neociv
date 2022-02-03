840f723823119d7743beaac385bdbff9dedca883
murt <murt@unpossible.io>
2022-02-03 17:23:08 +1000

    #2: Basic state outline

--- packages/neociv-game/src/lib.rs
+++ packages/neociv-game/src/lib.rs
@@ -2,10 +2,26 @@ use bevy::prelude::{App,Plugin};
 use bevy::core::CorePlugin;
 use bevy::input::InputPlugin;
 
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub enum GameState {
+    Init,
+    Menu,
+    Loading,
+    Playing,
+    Paused,
+    Credits,
+}
+
 pub struct NeocivGamePlugin;
 
 impl Plugin for NeocivGamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugin(CorePlugin::default()).add_plugin(InputPlugin::default());
+        app
+            // Setup Bevy
+            .add_plugin(CorePlugin::default()).add_plugin(InputPlugin::default())
+            // Init
+            .add_state(GameState::Init)
+            // Main Menu
+            .add_state(GameState::Menu);
     }
 }
