32a833bd47f2c41edf7d33e3a40d7c602ae79023
murt <murt@unpossible.io>
2022-08-08 16:59:54 +1000

    #2: Updated to bevy 0.8

--- packages/neociv-game/src/grid/camera.rs
+++ packages/neociv-game/src/grid/camera.rs
@@ -3,7 +3,7 @@ use bevy_mod_picking::PickingCameraBundle;
 
 pub fn setup_grid_camera(mut commands: Commands) {
     commands
-        .spawn_bundle(PerspectiveCameraBundle {
+        .spawn_bundle(Camera3dBundle {
             transform: Transform::from_xyz(0.0, 0.0, 30.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..Default::default()
         })
