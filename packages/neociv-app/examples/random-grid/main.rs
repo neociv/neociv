d005e1ec2fa3d6f103c362f7874e8d32e61bdd83
murt <murt@unpossible.io>
2022-09-22 20:48:39 +1000

    #2: Fixed random-grid example

--- packages/neociv-app/examples/random-grid/main.rs
+++ packages/neociv-app/examples/random-grid/main.rs
@@ -4,6 +4,7 @@ extern crate neociv_game as game;
 extern crate neociv_state;
 
 use neociv_state::engine;
+use bevy::math::Vec3;
 
 fn main() {
     let mut app = app::init_desktop_app(config::NeocivConfig::default());
@@ -12,10 +13,10 @@ fn main() {
     let mut state = engine::init();
 
     // Generate a 5 x 5 unowned grid
-    state = match engine::set_grid_size(state, 25, 25) {
-        Ok(s) => s,
-        Err(ex) => panic!("{:?}", ex),
-    };
+    state = engine::set_grid_size(state, 25, 15).unwrap();
+
+    // Correctly position the camera
+    state = engine::mod_camera_position(state, Vec3::new(0.0, 0.0, 100.0)).unwrap();
 
     // Add the state resource - the runtime setup will detect and instead use this instance
     app.insert_resource(state);
