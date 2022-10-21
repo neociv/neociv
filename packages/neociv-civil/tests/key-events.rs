16199efb42beb6296947d269d2c400a450482e2b
murt <murt@unpossible.io>
2022-10-21 16:47:24 +1000

    Updated dependencies for bevy@0.8.1

--- packages/neociv-civil/tests/key-events.rs
+++ packages/neociv-civil/tests/key-events.rs
@@ -1,11 +1,9 @@
 use neociv_civil::runtime::NeocivRuntime;
-use rlua::{Nil as LuaNil};
 
 #[test]
 fn from_lua() {
     let cvl = NeocivRuntime::default();
     let load_result = cvl.dofile_lua::<()>("./tests/resources/key-events.lua");
-    //panic!("{:?}", load_result.err());
     assert!(load_result.is_ok());
 }
 
@@ -13,6 +11,5 @@ fn from_lua() {
 fn from_cvl() {
     let cvl = NeocivRuntime::default();
     let load_result = cvl.dofile::<()>("./tests/resources/key-events.cvl");
-    //panic!("{:?}", load_result.err());
     assert!(load_result.is_ok());
 }
