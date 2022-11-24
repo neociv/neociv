0caea28b4c74517008500fd2950eb5700ed5e774
murt <murt@unpossible.io>
2022-11-24 20:42:30 +1000

    Added StateTable support to StateUnits

--- packages/neociv-state/src/unit/mod.rs
+++ packages/neociv-state/src/unit/mod.rs
@@ -1,11 +1,18 @@
+use serde::{Serialize, Deserialize};
+use serde_diff::SerdeDiff;
+
+use neociv_macros::StateTable;
+
 pub type UnitId = String;
 pub type UnitKey = String;
 
+#[derive(Clone, Debug, Serialize, Deserialize, SerdeDiff, StateTable)]
 pub struct StateUnit {
     pub id: UnitId,
     pub title: String,
 }
 
+#[derive(Clone, Debug, Serialize, Deserialize, SerdeDiff, StateTable)]
 pub struct StateUnitEntity {
     pub id: UnitId,
     pub key: UnitKey,
